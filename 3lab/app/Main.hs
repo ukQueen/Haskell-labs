632c4a28e032fef803492e9c7551203080c7ebae
Igor <gladkovigor2004@gmail.com>
2024-12-02 18:17:36 +0300

    3 lab is done

--- 3lab/app/Main.hs
+++ 3lab/app/Main.hs
@@ -2,96 +2,182 @@ module Main (main) where
 
 import Lib
 import Codec.Picture
-import Codec.Picture.Bitmap
-import qualified Data.ByteString as B
-import qualified Data.ByteString.Char8 as BC
 import System.IO
 import Control.DeepSeq (deepseq)
-
+import Control.Exception (IOException, try)
 
 
 main :: IO ()
 main = do
-    let text = "aHello World!"
-        key = "girl2"
-        alphabet = createAlphabet []
-        newAlphabet = createAlphabet "A"
-        encodedText = encoding key text 
-        binaryEncodedText = binaryEncoding  encodedText 
-    putStrLn alphabet
-    putStrLn newAlphabet
- --   putStrLn text
---    putStrLn encodedText
- --   putStrLn binaryEncodedText
-    --putStrLn $ takeCurrentSize binaryEncodedText 100 
+    menu 
 
-    --let img = binaryStringToImage binaryEncodedText 5 5
-    --savePngImage ("files/" ++ key ++ ".bmp") (ImageRGB8 img)
+menu :: IO()
+menu = do
+    putStrLn "Choose the action:"
+    putStrLn "[1] - encoding text into an image"
+    putStrLn "[2] - decoding text from the image"
+    putStrLn "[3] - encrypting text"
+    putStrLn "[4] - decrypting text"
+    putStrLn "[0] - Bye!"
+    input <- getLine
+    if null input  -- Проверка на пустой ввод
+        then do
+            putStrLn "You must choose a valid option. Please try again."
+            menu
+        else do
+            let n = read input :: Int
+            case n of    
+                1 -> do 
+                    menuEncoding
+                    putStrLn "---------------------------------"
+                    menu
+                2 -> do 
+                    menuDecoding
+                    putStrLn "---------------------------------"
+                    menu
+                3 -> do
+                    menuEncrypting
+                    putStrLn "---------------------------------"
+                    menu
+                4 -> do
+                    menuDecrypting
+                    putStrLn "---------------------------------"
+                    menu
+                0 -> putStrLn "Exiting..."
+                _ -> do
+                    putStrLn "Invalid choice. Try again."
+                    putStrLn "---------------------------------"
+                    menu
 
-    --content <- readFile "files/Alonzo Church's Biography.txt"
-    --let binaryContext = binaryEncoding $ encoding key content
-    --let img = binaryStringToImage binaryEncodedText 500 500
-    --savePngImage ("files/" ++ key ++ ".bmp") (ImageRGB8 img)
+
+menuEncoding:: IO()
+menuEncoding = do
+    putStrLn "write path to .txt file, that you want to encode (default \"files\\Alonzo Church's Biography.txt\")"
+    input <- getLine
+    let fileName = if null input then  "files\\Alonzo Church's Biography.txt" else input :: String
+    openning <- try (readFile fileName) :: IO (Either IOException String)
+    case openning of
+        Left ex -> putStrLn $ "Failed to open file: " ++ show ex
+        Right text -> do
+            putStrLn "Write Key for encoding (default \"Haskell\"):"
+            input1 <- getLine
+            let key = if null input1 then "Haskell" else read input1 :: String
+
+            putStrLn "Write width of image (default 30):"
+            input2 <- getLine
+            let width = if null input2 then 30 else read input2 :: Int
+            
+            putStrLn "Write height of image (default 30):"
+            input3 <- getLine
+            let height = if null input3 then 30 else read input3 :: Int
+                binaryEncodedText = binaryEncoding $ encoding key text
+                img = binaryStringToImage binaryEncodedText width height
+                img' = ImageRGB8 img
+                filePath = "files/" ++ key ++ ".bmp"
+            saveBmpImage filePath img'
+            putStrLn $ "Image saved to \"" ++ filePath ++ "\""
 
 
-    -- putStrLn "Читаем изображение..."
-    -- eimg <- readImage "files\\Alonzo_Church.bmp"
-    -- case eimg of
-    --     Left err -> putStrLn ("Ошибка чтения изображения: " ++ err)
-    --     Right dynImg -> do 
-    --         putStrLn "Изображение прочитано, конвертируем в RGB8..."
-    --         let img = convertRGB8 dynImg   
-    --             height = imageHeight img
-    --             width = imageWidth img
-    --             --putStrLn $ imageToBinary img
-    --         putStrLn "Изображение конвертировано. Генерируем бинарные данные..."
-    --         -- let newBinary = imageToBinary img
-    --         content <- readFile "files/Alonzo Church's Biography.txt"
-    --         let binaryContext = binaryEncoding $ encoding key content
-    --         putStrLn $ "Длина бинарных данных: " ++ show (length binaryContext)
-    --    --     putStrLn $ takeCurrentSize binaryContext 50 
-
-    --         putStrLn "Создаем новое изображение..."
-    --         let img' = binaryStringToImage binaryContext width height
-    --             dynImg' = ImageRGB8  img'
-    --         putStrLn "Сохраняем изображение..."
-    --         let filePath = "files/" ++ key ++ ".bmp"
-    --         saveImage filePath img'
-    --         putStrLn "BMP изображение успешно сохранено!"
-
-    putStrLn "Читаем изображение..."
-    eimg <- readImage "files\\Alonzo_Church.bmp"
+menuDecoding:: IO()
+menuDecoding = do
+    putStrLn "write path to the image, that you want to decode (default \"files\\Haskell.bmp\")"
+    input <- getLine
+    let path = if null input then  "files\\Haskell.bmp" else input :: String
+        key = getKey path
+    eimg <- readImage path
     case eimg of
-        Left err -> putStrLn ("Ошибка чтения изображения: " ++ err)
+        Left err -> putStrLn $ "Failed to open image: " ++ err
         Right dynImg -> do 
-            putStrLn "Изображение прочитано, конвертируем в RGB8..."
             let img = convertRGB8 dynImg   
+                bStr = imageToBinary img
+                binaryDecodedText = binaryDecoding bStr
+                decodedText = decoding key binaryDecodedText
+            putStrLn $ "write path to save decoded image (default \"files\\" ++ key ++ ".txt\")"
+            input1 <- getLine
+            let fileName = if null input1 then  "files\\" ++ key ++ ".txt" else input1 :: String
+            file <- openFile fileName WriteMode
+            hPutStr file decodedText
+            hClose file
+            putStrLn $ "Decoded image saved to " ++ fileName
+
+
+
+check :: Int -> IO Int
+check count  = 
+    if count >= 0 && count <= 8 then do return count
+    else do
+      putStrLn ("Input error. Try again")
+      input <- getLine
+      let count' = read input :: Int
+      check count'
+
+
+menuEncrypting :: IO()
+menuEncrypting = do
+    putStrLn "write path to the image (default \"files\\Alonzo_Church.bmp\")"
+    input <- getLine
+    let path = if null input then  "files\\Alonzo_Church.bmp" else input :: String
+        key = getKey path
+    eimg <- readImage path
+    case eimg of
+        Left err -> putStrLn $ "Failed to open image: " ++ err
+        Right dynImg -> do 
+            let img = convertRGB8 dynImg   
+                binaryImage = imageToBinary img
                 height = imageHeight img
                 width = imageWidth img
-                --putStrLn $ imageToBinary img
-            putStrLn "Изображение конвертировано. Генерируем бинарные данные..."
-            let newBinary = imageToBinary img
-            putStrLn $ "Длина бинарных данных: " ++ show (length newBinary)
-            putStrLn "Создаем новое изображение..."
-            let img' = binaryStringToImage newBinary width height
-            img' `deepseq` putStrLn "Изображение полностью сгенерировано!"
-            let dynImg' = ImageRGB8 img'
-            putStrLn "Сохраняем изображение..."
-            let filePath = "files/" ++ key ++ ".bmp"
-            --saveImage filePath img'
-            --savePngImage filePath dynImg'
 
-            putStrLn $ "Пиксель (0, 0): " ++ show (pixelAt img' 0 0)
-            putStrLn $ "Пиксель (width-1, height-1): " ++ show (pixelAt img' (width-1) (height-1))
+            putStrLn "write path to .txt file (default \"files\\Alonzo Church's Biography.txt\")"
+            inputF <- getLine
+            let fileName = if null inputF then  "files\\Alonzo Church's Biography.txt" else inputF :: String
+            openning <- try (readFile fileName) :: IO (Either IOException String)
+            case openning of
+                Left ex -> putStrLn $ "Failed to open file: " ++ show ex
+                Right text -> do
+                    putStrLn "Write Key for encoding (default \"Haskell\"):"
+                    input1 <- getLine
+                    let key = if null input1 then "Haskell" else input1 :: String
 
-            -- let img'' = generateImage (\_ _ -> PixelRGB8 255 0 0) 500 500
-            -- -- Сохраняем его как BMP
-            saveBmpImage filePath dynImg'
-            putStrLn "BMP изображение успешно сохранено!"
+                    putStrLn "Write count of bits (default 4):"
+                    input2 <- getLine
+                    let inp = if null input2 then 4 else read input2 :: Int
+                    count <- check inp 
+                    let binaryEncodedText = binaryEncoding $ encoding key text
+                        encryptedText = encrypting binaryImage binaryEncodedText count
+                        img' = binaryStringToImage encryptedText width height
+                        img'' = ImageRGB8 img'
+                    putStrLn "text is encrypting..."
+                    encryptedText `deepseq` putStrLn "text encrypted..."
+                    putStrLn "image is saving..."
+                    let filePath = "files/" ++ key ++ "_" ++ show count ++ ".bmp"
+                    saveBmpImage filePath img''
+                    putStrLn $ "Image saved to \"" ++ filePath ++ "\""
 
 
-saveImage :: FilePath -> Image PixelRGB8 -> IO ()
-saveImage filePath img = do
-    putStrLn "Сохраняем изображение как PNG..."
-    savePngImage filePath (ImageRGB8 img)
-    putStrLn "Изображение успешно сохранено!"
+menuDecrypting :: IO()
+menuDecrypting = do
+    putStrLn "write path to the image (default \"files\\Haskell_4.bmp\")"
+    input <- getLine
+    let path = if null input then  "files\\Haskell_4.bmp" else input :: String
+        key = getKey path
+    eimg <- readImage path
+    case eimg of
+        Left err -> putStrLn $ "Failed to open image: " ++ err
+        Right dynImg -> do 
+            let img = convertRGB8 dynImg   
+                binaryImage = imageToBinary img
+                (key, bits) = getKeyBits path
+                bText = decrypting binaryImage bits
+                binaryDecodedText = binaryDecoding bText
+                decodedText = decoding key binaryDecodedText
+            putStrLn $ "write path to save decrypted image (default \"files\\decrypdedImage.txt\")"
+            input1 <- getLine
+            let fileName = if null input1 then "files\\decrypdedImage.txt" else input1 :: String
+
+            putStrLn "image is decrypting..."
+            bText `deepseq` putStrLn "image decrypted..."
+            putStrLn "text is saving..."
+            file <- openFile fileName WriteMode
+            hPutStr file decodedText
+            hClose file
+            putStrLn $ "Decrypted image saved to \"" ++ fileName ++ "\""
--- 3lab/src/Lib.hs
+++ 3lab/src/Lib.hs
@@ -1,16 +1,20 @@
 module Lib
-    ( encoding, binaryEncoding, createAlphabet, removeDublicates, imageToBinary, binaryStringToImage, takeCurrentSize
+    ( encoding, decoding, binaryEncoding, binaryDecoding, imageToBinary, binaryStringToImage,  getKey, getKeyBits, encrypting, decrypting
     ) where
 
-import Numeric (showIntAtBase, readHex)
+import Numeric (showIntAtBase)
 import Data.Char (ord, intToDigit)
 import Data.Word (Word8)
 import Codec.Picture
-import qualified Data.ByteString as B
-import qualified Data.ByteString.Char8 as BC
 import Data.Char (digitToInt)
 import qualified Data.Vector as V
+import qualified Data.Vector.Mutable as MV
 import Data.Vector (Vector)
+import Data.Char (chr)
+import Control.Monad.ST
+
+
+
 
 
 -- убирает дубликаты в тексте
@@ -23,7 +27,7 @@ removeDublicates = removeDublicates' []
       | x `elem ` seen = removeDublicates' seen xs
       | otherwise = x : removeDublicates' (x:seen) xs
 
---функция которая используется в кодировании символов (сопоставляет символ из обыного алфавита в новый) 
+-- конвертирует символы из одного алфавита в другой
 convertText :: String -> String -> String -> String
 convertText [] _ _ = []
 convertText (c:cs) alphabet newAlphabet =
@@ -48,24 +52,54 @@ encoding key text =
         newAlphabet = createAlphabet key
     in convertText text alphabet newAlphabet
 
+
+-- декодирует текст с помощью кодировки кодовым словом
+decoding :: String -> String -> String
+decoding key text = 
+    let alphabet = createAlphabet key
+        defaultAlphabet = createAlphabet []
+    in convertText text alphabet defaultAlphabet 
+
 -- число в двоичную строку
 toBinary :: Int -> Vector Char 
 toBinary n = V.fromList (replicate (8 - length bin) '0') V.++ V.fromList bin
     where bin = showIntAtBase 2 intToDigit n ""
 
-
--- код из ASCII символа в двоичную строку
+-- число из двоичной строки
+fromBinary :: Vector Char -> Int 
+fromBinary str 
+    | V.null str = 0 
+    |otherwise = num + fromBinary str'
+        where 
+            num = if V.head str == '1' then 2 ^ (length str - 1) else 0 
+            str' = V.tail str
+          
+-- преобразовывает символ в двоичную строку
 charToBinary :: Char -> Vector Char
-charToBinary c = toBinary (ord c)
+charToBinary c = toBinary $ ord c
+
+-- получает символ из двоичной строки
+charFromBinary :: Vector Char -> Char
+charFromBinary c =  chr $ fromBinary c 
 
 -- текст в двоичный код
 binaryEncoding :: String -> Vector Char
 binaryEncoding [] = V.empty
 binaryEncoding (c:cs) = charToBinary c V.++ binaryEncoding cs
 
+
+-- текст из двоичного кода
+binaryDecoding :: Vector Char -> String
+binaryDecoding str
+    | V.null str = []
+    | V.length str < 8 = []
+    | otherwise = charFromBinary (V.take 8 str) : binaryDecoding (V.drop 8 str)
+
+
 -- пиксель в двоичный код
 pixelToBinary :: PixelRGB8 -> Vector Char
 pixelToBinary (PixelRGB8 r g b) = toBinary (fromIntegral r) V.++ toBinary (fromIntegral g) V.++ toBinary (fromIntegral b) 
+
 
 -- изображение в двоичный код
 imageToBinary :: Image PixelRGB8 -> Vector Char
@@ -74,6 +108,7 @@ imageToBinary img = V.concat [pixelToBinary (pixelAt img x y) | y <- [0..height-
         width = imageWidth img
         height = imageHeight img
     
+
 -- дополняет двоичную строку нулями до вводимого размера
 takeCurrentSize :: Vector Char -> Int -> Vector Char
 takeCurrentSize str size 
@@ -85,14 +120,11 @@ takeCurrentSize str size
 takeAndDrop :: Int -> Vector Char ->(Vector Char, Vector Char)
 takeAndDrop n arr = (V.take n arr, V.drop n arr)
 
+
 -- двоичную строку преобразует в цвет (8 битов в r, g или b )
 binaryStringToColor :: Vector Char -> Word8
 binaryStringToColor str = fromIntegral $ foldl (\acc x -> acc * 2 + digitToInt x) 0 str
 
--- делит на куски по n элементов массив arr 
-chunksOf :: Int -> [a] -> [[a]]
-chunksOf _ [] = []
-chunksOf n arr = take n arr : chunksOf n (drop n arr)
 
 -- двочиную строку превращает в изображение
 binaryStringToImage :: Vector Char -> Int -> Int -> Image PixelRGB8
@@ -108,15 +140,98 @@ binaryStringToImage str width height =
             in PixelRGB8 r g b
     in generateImage pixelRenderer width height
 
--- binaryStringToImage :: String -> Int -> Int -> Image PixelRGB8
--- binaryStringToImage str width height =
---     let size = height * width * 3 * 8
---         newStr = takeCurrentSize str size
---         precomputed = map binaryStringToColor (chunksOf 8 newStr)
---         pixelRenderer x y =
---             let index = (y * width + x) * 3
---                 r = precomputed !! index
---                 g = precomputed !! (index + 1)
---                 b = precomputed !! (index + 2)
---             in PixelRGB8 r g b
---     in generateImage pixelRenderer width height
+-- достает название файла из пути
+getKey :: String -> String
+getKey path = 
+    takeWhile (/= '.') (reverse (takeWhile (\c -> c /= '\\' && c /= '/') (reverse path)))
+
+-- encrypting :: Vector Char -> Vector Char -> Int -> Vector Char
+-- encrypting img str count
+--     | V.null img = V.empty
+--     | otherwise =  
+--         let currentStr = takeCurrentSize str (V.length img `div` 8 * count)
+--             img' = V.drop 8 img
+--             currentStr' = V.drop count currentStr
+--             prefix = V.take (8 - count) img
+--             suffix = V.take count currentStr
+--             rest = encrypting img' currentStr' count
+--         -- in V.take (8 - count) img V.++ V.take count currentStr V.++ encrypting img' currentStr' count 
+--         in V.concat[prefix, suffix, rest]
+
+-- encrypting :: Vector Char -> Vector Char -> Int -> Vector Char
+-- encrypting img str count = runST $ do
+--     let indx = 0
+--         len = length img
+--         str' = takeCurrentSize str (len `div` 8 * count)
+--     result <- MV.new len 
+--     let go indx
+--             | indx * 8 >= len  = return ()
+--             | otherwise = do
+--                 let elem0 = if count == 8 then str' V.! (indx * count)  else img V.! (indx * 8)
+--                     elem1 = if count >= 7 then str' V.! (indx * count + 1 - (8 - count)) else img V.! (indx * 8 + 1)
+--                     elem2 = if count >= 6 then str' V.! (indx * count + 2 - (8 - count)) else img V.! (indx * 8 + 2)
+--                     elem3 = if count >= 5 then str' V.! (indx * count + 3 - (8 - count)) else img V.! (indx * 8 + 3)
+--                     elem4 = if count >= 4 then str' V.! (indx * count + 4 - (8 - count)) else img V.! (indx * 8 + 4)
+--                     elem5 = if count >= 3 then str' V.! (indx * count + 5 - (8 - count)) else img V.! (indx * 8 + 5)
+--                     elem6 = if count >= 2 then str' V.! (indx * count + 6 - (8 - count)) else img V.! (indx * 8 + 6)
+--                     elem7 = if count >= 1 then str' V.! (indx * count + 7 - (8 - count)) else img V.! (indx * 8 + 7)
+--                 MV.write result (indx * 8) elem0
+--                 MV.write result (indx * 8 + 1) elem1
+--                 MV.write result (indx * 8 + 2) elem2
+--                 MV.write result (indx * 8 + 3) elem3
+--                 MV.write result (indx * 8 + 4) elem4
+--                 MV.write result (indx * 8 + 5) elem5
+--                 MV.write result (indx * 8 + 6) elem6
+--                 MV.write result (indx * 8 + 7) elem7
+--                 go (indx + 1)
+--     go 0
+--     V.freeze result
+
+
+
+encrypting :: Vector Char -> Vector Char -> Int -> Vector Char
+encrypting img str count = runST $ do
+    let indx = 0
+        len = length img
+        str' = takeCurrentSize str (len `div` 8 * count)
+    result <- MV.new len 
+    let go indx
+            | indx * 8 >= len  = return ()
+            | otherwise = do
+                V.forM_ (V.fromList [0..7]) $ \i -> do
+                    let elem = if count >= (8 - i)
+                               then str' V.! (indx * count + i - (8 - count))
+                               else img V.! (indx * 8 + i)
+                    MV.write result (indx * 8 + i) elem
+                go (indx + 1)
+    go 0
+    V.freeze result
+
+
+getKeyBits :: String -> (String, Int)
+getKeyBits path = 
+    let fileName = takeWhile (/= '.') (reverse (takeWhile (\c -> c /= '\\' && c /= '/') (reverse path)))
+        key = takeWhile (/= '_') fileName
+        bits = takeWhile (/= '.') (filter  (/= '_') (dropWhile (/= '_') fileName))
+        bits' = read bits::Int
+    in (key, bits')
+
+
+decrypting :: Vector Char -> Int -> Vector Char
+decrypting img count = runST $ do
+    let indx = 0
+        len = length img
+    result <- MV.new (len `div` 8 * count) 
+    let go indx
+            | indx * 8 >= len  = return ()
+            | otherwise = do
+                V.forM_ (V.fromList [0..7]) $ \i -> do
+                    let elem = if count >= (8 - i)
+                               then img V.! (indx * 8 + i )
+                               else '\0'
+                    if elem /= '\0' 
+                        then MV.write result (indx * count + i - (8 - count)) elem
+                        else return ()
+                go (indx + 1)
+    go 0
+    V.freeze result
